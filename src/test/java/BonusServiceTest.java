2f4193d05a8de9fc7cb01b70e949043b4ee08639
Neto test <zilya.zakirova1991@gmail.com>
2023-03-29 16:23:29 +0300

    Change project

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+    public class BonusServiceTest {
+
+        @Test
+        void shouldCalculateForRegisteredAndUnderLimit() {
+            BonusService service = new BonusService();
+
+            // подготавливаем данные:
+            long amount = 1000;
+            boolean registered = true;
+            long expected = 30;
+
+            // вызываем целевой метод:
+            long actual = service.calculate(amount, registered);
+
+            // производим проверку (сравниваем ожидаемый и фактический):
+            Assertions.assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldCalculateForRegisteredAndOverLimit() {
+            BonusService service = new BonusService();
+
+            // подготавливаем данные:
+            long amount = 1_000_000;
+            boolean registered = true;
+            long expected = 500;
+
+            // вызываем целевой метод:
+            long actual = service.calculate(amount, registered);
+
+            // производим проверку (сравниваем ожидаемый и фактический):
+            Assertions.assertEquals(expected, actual);
+        }
+    }
+
